d538f37a9b9deceee273c6cbf594c0bb2e24f38f
Alek Tobias <alektobias@gmail.com>
2023-10-05 08:01:42 -0300

    feat: database for backend added

---
+++ backend/prisma/migrations/20231005005415_create_favorite_colors/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "FavoriteColor" (
+    "cpf" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "color" TEXT NOT NULL,
+
+    CONSTRAINT "FavoriteColor_pkey" PRIMARY KEY ("cpf")
+);
