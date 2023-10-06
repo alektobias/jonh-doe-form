14169fe6b9cc23550a95d774955d1ef02db6b2ed
Alek Tobias <alektobias@gmail.com>
2023-10-05 22:07:46 -0300

    fix: cleanup files

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,24 +2,14 @@ import './App.css'
 import { useCallback, useState } from 'react'
 
 import { client } from './clients/axios'
-import { Icon } from '@iconify/react';
 import FavoriteColorForm, { FormSchemaType } from './components/FavoriteColorForm'
 import { SubmitHandler } from 'react-hook-form';
 import SuccessMessage from './components/SuccessMessage';
 import FailureMessage from './components/FailureError';
 
-
-
-
-
-
-
 function App() {
 
   const [submitStatus, setSubmitStatus] = useState<{ status: 'filling' | 'success' | 'failure', message?: string }>({ status: 'filling' })
-
-
-
 
   const onSubmit: SubmitHandler<FormSchemaType> = useCallback(
     (data) => {
